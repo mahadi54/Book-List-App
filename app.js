d87998ce1a8b1c9e78b69354b6614d5e38c66e23
mahadi54 <hmahadi505@gmail.com>
2022-01-13 11:05:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let list = document.querySelector('#book-list ul')
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        let li = e.target.parentElement;
+        list.removeChild(li)
+    }
+})
+let form = document.forms['add-book']
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let value = form.querySelector('input[type="text"]').value;
+    let li = document.createElement('li')
+    let bookName = document.createElement('span')
+    let deleteBtn = document.createElement('span')
+
+    bookName.textContent = value;
+    deleteBtn.textContent = 'delete';
+    bookName.classList.add('name')
+    deleteBtn.classList.add('delete')
+
+    li.appendChild(bookName)
+    li.appendChild(deleteBtn)
+    list.appendChild(li);
+    value = ""
+
+
+})
+
+let hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+    if(hideBox.checked){
+        list.style.display = "none"
+    } else{
+        list.style.display = "initial"
+    }
+})
